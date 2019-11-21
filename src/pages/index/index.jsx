baeb025cf56bb8aaa820f40fc2d7c58e1a1d54ad
shenghanqin <shenghanqin@hujiang.com>
2019-11-21 09:58:19 +0800

    update 2.0.0 beta版测试

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,6 +1,9 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
-import '@tarojs/async-await'
+// import TaroCanvasDrawer from 'taro-plugin-canvas'
+import {
+  TaroCropper
+} from 'taro-cropper';
 import './index.styl'
 
 export default class Index extends Component {
@@ -9,10 +12,23 @@ export default class Index extends Component {
     navigationBarTitleText: '首页'
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      canvasConfig: {
+        width: 300,
+        height: 300
+      },
+      cutImagePath: ''
+    }
+  }
+
   componentWillMount () { }
 
   componentDidMount () {
     this.getLoginInfo()
+    Taro.setStorageSync('abc', 123)
+    console.log('Taro :', Taro);
   }
 
   getLoginInfo = async () => {
@@ -20,10 +36,32 @@ export default class Index extends Component {
     console.log('settings :', settings)
   }
 
+  onCreateSuccess = (e) => {
+    console.log('onCreateSuccess e :', e);
+  }
+  onCreateFail = (e) => {
+    console.log('onCreateFail e :', e);
+  }
+
+  onCut = res => {
+    this.setState({
+      cutImagePath: res
+    })
+  }
+
   render () {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <Text>Hello world!{this.state.cutImagePath}</Text>
+        <TaroCropper
+          fullScreen
+          onCut={this.onCut}
+        />
+        {/* <TaroCanvasDrawer
+          config={this.state.canvasConfig}
+          onCreateSuccess={this.onCreateSuccess}
+          onCreateFail={this.onCreateFail}
+        /> */}
       </View>
     )
   }
